e29f73bbd885247272827978a90daeb714f118a5
Sandeep LS <110336567+SandeepLS@users.noreply.github.com>
2024-01-19 08:43:01 +0530

    add routes and auth page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+import Signin from './Pages/Signin';
+import Signup from './Pages/Signup';
+import Home from './Pages/Home';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
